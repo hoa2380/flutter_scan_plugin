6f2714035d63982e53e0ffbaa2e89e9bf9461c50
hoa2380 <hoagamer2k@gmail.com>
2023-07-31 15:58:49 +0700

    commit

--- lib/flutter_scan_plugin.dart
+++ lib/flutter_scan_plugin.dart
@@ -1,8 +1,20 @@
+import 'package:cuervo_document_scanner/cuervo_document_scanner.dart';
 
 import 'flutter_scan_plugin_platform_interface.dart';
+enum Type { CAMERA, GALLERY }
 
 class FlutterScanPlugin {
+
   Future<String?> getPlatformVersion() {
     return FlutterScanPluginPlatform.instance.getPlatformVersion();
   }
+
+  static Future<List<String>?> start(Type type) async {
+    if (type == Type.CAMERA) {
+      return CuervoDocumentScanner.getPictures(Source.CAMERA);
+    } else {
+      return CuervoDocumentScanner.getPictures(Source.GALLERY);
+    }
+  }
 }
+
