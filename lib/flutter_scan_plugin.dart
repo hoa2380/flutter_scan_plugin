18bd18986985fa005d2163dd2fe9cbf3546b0a6c
hoa2380 <hoagamer2k@gmail.com>
2023-07-31 16:18:06 +0700

    update

--- lib/flutter_scan_plugin.dart
+++ lib/flutter_scan_plugin.dart
@@ -1,4 +1,5 @@
 import 'package:cuervo_document_scanner/cuervo_document_scanner.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'flutter_scan_plugin_platform_interface.dart';
 enum Type { CAMERA, GALLERY }
@@ -11,10 +12,31 @@ class FlutterScanPlugin {
 
   static Future<List<String>?> start(Type type) async {
     if (type == Type.CAMERA) {
-      return CuervoDocumentScanner.getPictures(Source.CAMERA);
+      return _getPicturesFromCamera();
     } else {
-      return CuervoDocumentScanner.getPictures(Source.GALLERY);
+      return _getPicturesFromGallery();
     }
+  }
+
+  static Future<List<String>?> _getPicturesFromCamera() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.camera,
+    ].request();
+    if (statuses.containsValue(PermissionStatus.denied)) {
+      throw Exception("Permission not granted");
+    }
+    return CuervoDocumentScanner.getPictures(Source.CAMERA);
+  }
+
+  static Future<List<String>?> _getPicturesFromGallery() async {
+    Map<Permission, PermissionStatus> statuses = await [
+      Permission.photos,
+    ].request();
+    if (statuses.containsValue(PermissionStatus.denied)) {
+      throw Exception("Permission not granted");
+    }
+
+    return CuervoDocumentScanner.getPictures(Source.CAMERA);
   }
 }
 
